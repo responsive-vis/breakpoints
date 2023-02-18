3e69fb4d1e78696033df72e8b351d9fbe3935bc7
Sarah Schöttler <sarah@schoettler.email>
2023-02-18 13:55:17 +0000

    install dsv plugin, update svelte kit

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import dsv from '@rollup/plugin-dsv';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit(), dsv()]
 });
