3a01636b4e5075efada59349d7c76ee76981757c
Sarah Schöttler <sarah@schoettler.email>
2023-02-19 14:41:18 +0000

    styling

--- src/lib/components/StatusBar.svelte
+++ src/lib/components/StatusBar.svelte
@@ -13,11 +13,13 @@
 </script>
 
 <div id="controls">
-	<em>
-		Resize the container using the handle in the botton right and watch the visualization update
-		(Chrome/Firefox only).
-	</em>
-	<br />
+	<p>
+		<em>
+			Resize the container using the handle in the botton right and watch the visualization update
+			(Chrome/Firefox only).
+		</em>
+	</p>
+
 	<ul>
 		<li>Width: {width} px</li>
 		<li>Height: {height} px</li>
@@ -25,28 +27,50 @@
 		<li>Area: {divArea} px</li>
 		<li />
 	</ul>
-	<br />
-	<input type="checkbox" id="show-landscape-overlay" bind:checked={landscapeOverlay} /><label
-		for="show-landscape-overlay">Show view landscape overlay</label
-	>
-	<button id="export-landscape" on:click={window.open(viewLandscape.image)}>Download as PNG</button>
+
+	<p>
+		<input type="checkbox" id="show-landscape-overlay" bind:checked={landscapeOverlay} /><label
+			for="show-landscape-overlay">Show view landscape overlay</label
+		>
+		<button id="export-landscape" on:click={window.open(viewLandscape.image)}
+			>Download as PNG</button
+		>
+	</p>
 </div>
 
 <style>
 	#controls {
-		line-height: 1em;
+		/* line-height: 1rem; */
+		margin-bottom: 0.3rem;
+	}
+	p,
+	ul {
+		margin: 0;
+		line-height: 1.3rem;
 	}
 	input {
 		margin: 0 5px 7px 0;
 	}
 	ul {
 		list-style-type: none;
-		margin: 5px 0;
+		margin: 0;
 		padding: 0;
 		display: inline-block;
 	}
 	li {
 		display: inline-block;
 		width: 150px;
+	}
+	button {
+		background-color: white;
+		color: black;
+		border: 2px solid #bebada;
+		border-radius: 2rem;
+		padding: 0.25rem 0.5rem 0.15rem 0.5rem;
+		font-size: 0.8rem;
+		transition-duration: 0.3s;
+	}
+	button:hover {
+		background-color: #bebada;
 	}
 </style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,20 @@
 	import { base } from '$app/paths';
 </script>
 
+<svelte:head>
+	<style>
+		* {
+			font-family: sans-serif;
+		}
+
+		body,
+		html {
+			margin: 0;
+			padding: 0;
+		}
+	</style>
+</svelte:head>
+
 <div id="header">
 	<h1>Data-driven Breakpoints for Responsive Visualization</h1>
 	<nav>
@@ -17,15 +31,15 @@
 
 <style>
 	h1 {
-		font-size: larger;
-		margin: 5px 0;
+		font-size: 1.2rem;
+		margin: 0.4rem 0;
 	}
 	#header {
 		background-image: linear-gradient(to right, #bebada, #8dd3c7, #ffffb3);
 		display: block;
-		padding: 10px;
+		padding: 0.7rem;
 	}
 	#content {
-		padding: 10px;
+		padding: 0.7rem;
 	}
 </style>
